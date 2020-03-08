149ecb51cba9c4396637af484c13cf62c7a394d2
zivaev462 <z.ivaev@mail.ru>
2020-03-08 12:28:30 +0500

    Initial commit

---
+++ GitMobile/App Layer/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  GitMobile
+//
+//  Created by Zafar on 3/8/20.
+//  Copyright © 2020 Zafar. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '11.4'
+use_frameworks!
+
+target 'GitMobile' do
+pod 'RxSwift'
+pod 'RxCocoa'
+pod 'SnapKit'
+end
